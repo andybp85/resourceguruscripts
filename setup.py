82da1c0b4e7f1a99458921ece286751572863159
Andrew Stanish <andy@youshallthrive.com>
2015-05-15 15:49:00 -0400

    updating setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ config = {
     'version'      : '0.1',
     'description'  : 'ResourceGuru Python for Scripts',
     'author'       : 'Andrew Stanish for YouShallThrive, Inc. based on original by Owen Barton',
+    'author_email' : 'andy@youshallthrive.com',
     'url'          : 'https://github.com/andybp85/resourceguruscrips',
     'download_url' : 'https://github.com/andybp85/resourceguruscripts/tarball/0.1'
     'keywords'     : ['resourceguru'],
