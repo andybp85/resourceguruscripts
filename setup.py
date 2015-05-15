c4a0e7d8b2bf4d7aa6534955d217661c5d8db25b
Andrew Stanish <andy@youshallthrive.com>
2015-05-15 15:48:19 -0400

    updating setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'name': 'resourceguruscripts',
-    'packages': ['resourceguruscripts'],
-    'version': '0.1',
-    'description': 'ResourceGuru Python for Scripts',
-    'author': 'Andrew Stanish for YouShallThrive, Inc. based on original by Owen Barton',
-    'url': 'https://github.com/andybp85/resourceguruscrips',
-    'keywords' : ['resourceguru'],
-    'classifiers' : [],
-    'requires': ['requests_oauthlib']
+    'name'         : 'resourceguruscripts',
+    'packages'     : ['resourceguruscripts'],
+    'version'      : '0.1',
+    'description'  : 'ResourceGuru Python for Scripts',
+    'author'       : 'Andrew Stanish for YouShallThrive, Inc. based on original by Owen Barton',
+    'url'          : 'https://github.com/andybp85/resourceguruscrips',
+    'download_url' : 'https://github.com/andybp85/resourceguruscripts/tarball/0.1'
+    'keywords'     : ['resourceguru'],
+    'classifiers'  : [],
+    'requires'     : ['requests_oauthlib']
     }
 
 setup(**config)
