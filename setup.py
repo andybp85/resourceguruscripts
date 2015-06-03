7700b808823259626640f81699cbd087ee5dc27f
Andrew Stanish <andy@youshallthrive.com>
2015-06-03 19:15:44 -0400

    updating version numbers

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ except ImportError:
 config = {
     'name'         : 'resourceguruscripts',
     'packages'     : ['resourceguruscripts'],
-    'version'      : '0.8.2',
+    'version'      : '0.8.3',
     'description'  : 'ResourceGuru Python for Scripts',
     'author'       : 'Andrew Stanish for YouShallThrive, Inc. based on original by Owen Barton',
     'author_email' : 'andy@youshallthrive.com',
     'url'          : 'https://github.com/andybp85/resourceguruscripts',
-    'download_url' : 'https://github.com/andybp85/resourceguruscripts/tarball/0.8.2',
+    'download_url' : 'https://github.com/andybp85/resourceguruscripts/tarball/0.8.3',
     'keywords'     : ['resourceguru'],
     'classifiers'  : [],
     'requires'     : ['requests_oauthlib']
